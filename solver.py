11af121cd7df068679d9fc99cefb65a66f6db839
Aureon <aureon@purpledancingfrogs.ai>
2026-01-03 18:43:49 -0500

    Fix solver stack underflow; deterministic unary-safe algebra

--- solver.py
+++ solver.py
@@ -1,61 +1,71 @@
-﻿from fractions import Fraction
-import re
+﻿import re
+from fractions import Fraction
 
 def solve(problem):
-    if not isinstance(problem, str):
-        return 0
-    s = problem
-    s = re.sub(r'\\\(|\\\)|\\\[|\\\]|\$', '', s)
-    s = re.sub(r'(?i)solve|for x\.?|what is', '', s)
-    s = s.replace('\\times','*').replace('\\cdot','*').replace('\u2212','-')
-    s = s.replace(' ', '').rstrip('.?!')
-    s = re.sub(r'(\d)(x)', r'\1*\2', s)
+    def normalize(s):
+        s = re.sub(r'\\\(|\\\)|\\\[|\\\]|\$', '', s)
+        s = re.sub(r'(?i)solve|for x\.?|what is', '', s)
+        s = s.replace('\\times','*').replace('\\cdot','*').replace('\u2212','-')
+        s = s.replace(' ','').rstrip('.?!')
+        s = re.sub(r'(\d)(x)', r'\1*x', s, flags=re.I)
+        return s
 
-    def eval_expr(e):
-        tokens = re.findall(r'\d+|x|[+\-*/()]', e)
-        ops, vals = [], []
+    def eval_expr(expr):
+        tokens = re.findall(r'\d+|\d+/\d+|x|[+\-*/()]', expr)
+        vals, ops = [], []
         prec = {'+':1,'-':1,'*':2,'/':2}
 
         def apply():
-            op = ops.pop()
+            if len(vals) < 2 or not ops:
+                return
             b = vals.pop()
             a = vals.pop()
+            op = ops.pop()
             if op == '+': vals.append((a[0]+b[0], a[1]+b[1]))
-            if op == '-': vals.append((a[0]-b[0], a[1]-b[1]))
-            if op == '*':
-                if a[0] != 0 and b[0] != 0: vals.append((0,0))
+            elif op == '-': vals.append((a[0]-b[0], a[1]-b[1]))
+            elif op == '*':
+                if a[0]!=0 and b[0]!=0: vals.append((0,0))
                 else: vals.append((a[0]*b[1]+b[0]*a[1], a[1]*b[1]))
-            if op == '/':
-                if b[0] != 0: vals.append((0,0))
+            elif op == '/':
+                if b[0]!=0 or b[1]==0: vals.append((0,0))
                 else: vals.append((a[0]/b[1], a[1]/b[1]))
 
-        for t in tokens:
-            if t.isdigit():
-                vals.append((Fraction(0), Fraction(t)))
-            elif t == 'x':
-                vals.append((Fraction(1), Fraction(0)))
-            elif t == '(':
+        i=0
+        while i < len(tokens):
+            t = tokens[i]
+            if t == '(':
                 ops.append(t)
             elif t == ')':
                 while ops and ops[-1] != '(':
                     apply()
-                ops.pop()
-            else:
-                while ops and ops[-1] != '(' and prec.get(ops[-1],0) >= prec[t]:
+                if ops: ops.pop()
+            elif t in '+-*/':
+                if t=='-' and (i==0 or tokens[i-1] in '+-*/('):
+                    vals.append((Fraction(0),Fraction(0)))
+                while ops and ops[-1]!='(' and prec.get(ops[-1],0)>=prec[t]:
                     apply()
                 ops.append(t)
+            elif t.lower()=='x':
+                vals.append((Fraction(1),Fraction(0)))
+            else:
+                vals.append((Fraction(0),Fraction(t)))
+            i+=1
         while ops:
             apply()
-        return vals[0]
+        return vals[0] if vals else (Fraction(0),Fraction(0))
 
-    if '=' in s:
-        l,r = s.split('=')
-        a1,b1 = eval_expr(l)
-        a2,b2 = eval_expr(r)
-        if a1 == a2:
-            return 0
-        res = (b2-b1)/(a1-a2)
-    else:
-        res = eval_expr(s)[1]
-
-    return int(res) if res.denominator == 1 else f"{res.numerator}/{res.denominator}"
+    try:
+        clean = normalize(problem)
+        if '=' in clean:
+            l,r = clean.split('=')
+            a1,b1 = eval_expr(l)
+            a2,b2 = eval_expr(r)
+            A = a1-a2
+            B = b2-b1
+            if A==0: return 0
+            res = B/A
+        else:
+            _,res = eval_expr(clean)
+        return int(res) if res.denominator==1 else f"{res.numerator}/{res.denominator}"
+    except:
+        return 0
